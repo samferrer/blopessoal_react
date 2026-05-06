f3fe16951e29b4c3a95e9f45bb7e0d9723402cbc
Samara Ferreira <samaraferreira.costa@hotmail.com>
2026-05-06 17:27:02 -0300

    Adicionado navbar e footer

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,34 @@
 function Home() {
     return (
         <>
-            <div className="flex justify-center">
-                <h1 className="mr-6 bg-fuchsia-600">Teste</h1>
+            <div className="bg-indigo-900 flex justify-center">
+                <div className='container grid grid-cols-2 text-white'>
+                    <div className="flex flex-col gap-4 items-center justify-center py-4">
+                        <h2 className='text-5xl font-bold'>
+                            Seja Bem Vinde!
+                        </h2>
+
+                        <p className='text-xl'>
+                            Expresse aqui seus pensamentos e opiniões
+                        </p>
+
+                        <div className="flex justify-around gap-4">
+                            <div className='rounded text-white
+                                            border-white border-solid border-2 py-2 px-4'
+                            >
+                                Nova Postagem
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex justify-center ">
+                        <img
+                            src="https://i.imgur.com/fyfrilv.png"
+                            alt="Imagem Página Home"
+                            className='w-2/3'
+                        />
+                    </div>
+                </div>
             </div>
         </>
     )
