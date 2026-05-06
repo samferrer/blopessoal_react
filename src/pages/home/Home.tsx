1571fd55bc2f2e306513818c87e99c67544747bd
Samara Ferreira <samaraferreira.costa@hotmail.com>
2026-05-06 15:43:22 -0300

    blogpessoal react

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+        <>
+            <div className="flex justify-center">
+                <h1 className="mr-6 bg-fuchsia-600">Teste</h1>
+            </div>
+        </>
+    )
+}
+
+export default Home
